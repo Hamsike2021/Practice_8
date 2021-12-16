8de8f7c55b538a35db0e35478bc008662d3730a9
Ербулатов Хамит <90035385+Hamsike2021@users.noreply.github.com>
2021-12-16 22:22:48 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('indir', type=str)
+
+arg = parser.parse_args().indir
 try:
-  with open(input(), 'r', encoding='utf8') as r:
-    print(r.read(10))
+    with open(arg, 'r', encoding='utf8') as r:
+        print(r.read(10))
 except FileNotFoundError:
-  print('Error')
+    print('Error')
